581d522a67e359c76410b884a1224dc65c0e42be
Daniel Smith <714.daniel.smith@gmail.com>
2019-11-28 16:01:58 -0800

    added algos

--- graph-algorithms.py
+++ graph-algorithms.py
@@ -3,21 +3,99 @@ import matplotlib.pyplot as plt
 import numpy as np 
 import sys
 
+GRAPH_SIZE = 6
+
+def getWeight(e):
+	return G[e[0]][e[1]]['weight']
+
 #give each edge of a graph a random weight from 0 to 100
 def generate_random_weights(G):
 	for u,v in G.edges():
-			G[u][v]['weight'] = np.random.randint(100)
+		G[u][v]['weight'] = np.random.randint(100)
+
+def generate_triangle_inequality_weight(G):
+	for u,v in G.edges():
+		G[u][v]['weight'] = -1
+	for u in G.nodes():
+		for v in G[u]:
+			for w in G[v]:
+					if w != u:
+						if G[u][v]['weight'] < 0:
+							G[u][v]['weight']  = np.random.randint(1,100)
+						if (G[u][w]['weight'] > 0 and G[v][w]['weight'] > 0):
+							G[u][w]['weight'] = np.random.randint(1,G[u][v]['weight'] + G[v][w]['weight'])
+
+def nearest_neighbor(G,v):
+	visited = [False] * len(G.nodes)
+	visited[v] = True
+	curVertex = v
+	curSum = 0
+	path = list()
+
+	while False in visited:
+		lowest_weight = -1
+		lowest_weight_vertex = -1
+		for w in G[curVertex]:
+			if visited[w] == False:
+				if lowest_weight == -1 or G[curVertex][w]['weight'] < lowest_weight:
+					lowest_weight_vertex = w
+					lowest_weight = G[curVertex][w]['weight']
+		curVertex = lowest_weight_vertex
+		visited[lowest_weight_vertex] = True
+		curSum += lowest_weight
+		path.append(lowest_weight_vertex)
+	return [path, curSum]
+
+def find_mst_with_kruskal(G):
+	sorted_edges = list(G.edges())
+	sorted_edges.sort(key = getWeight)
+	mst = nx.Graph()
+	vertices = set()
+	all_vertices = set(G.nodes())
+	edge_index = 0
+	while vertices != all_vertices or not nx.is_connected(mst):
+		curEdge = sorted_edges[edge_index]
+		if not curEdge[0] in vertices or not curEdge[1] in vertices or not nx.has_path(mst,curEdge[0],curEdge[1]):
+			if curEdge[0] not in vertices:
+				vertices.add(curEdge[0])
+				mst.add_node(curEdge[0])
+			if curEdge[1] not in vertices:
+				vertices.add(curEdge[1])
+				mst.add_node(curEdge[1])		
+			mst.add_edge(*curEdge)
+			mst[curEdge[0]][curEdge[1]]['weight'] = getWeight(curEdge)
+		edge_index += 1
+	return mst
+
+def approx_path(G,u,v):
+	mst = find_mst_with_kruskal(G)
+	path = nx.all_simple_paths(mst,u,v)
+	sum = 0
+	for p in path:
+		for i in range(len(p) - 1):
+			sum += mst[p[i]][p[i + 1]]['weight']
+	print("APPROX IS ", p)
+	return sum
+
+
+
+
+
 
 #make and show a complete graph on 10 vertices
-def make_and_show_graph():
-	G = nx.complete_graph(10)
-	generate_random_weights(G)
+def make_graph():
+	G = nx.complete_graph(GRAPH_SIZE)
+	generate_triangle_inequality_weight(G)
+	#generate_random_weights(G)
+	return G
+
+def show_graph(G):
 	pos = nx.spring_layout(G)
 	weights = nx.get_edge_attributes(G,'weight')
 	nx.draw(G,pos)
 	nx.draw_networkx_labels(G, pos)
 	nx.draw_networkx_edge_labels(G,pos,weights, with_labels=True)
-	return G
+	plt.show()
 
 #find the smallest distance to a vertex not yet visited
 def find_smallest_distance(G, visited, dist):
@@ -46,8 +124,20 @@ def dijkstra_dist(G,originalVertex):
 							dist[v] = dist[nearest_neighbor] + G[nearest_neighbor][v]['weight']
 	return dist
 
-G = make_and_show_graph()
+G = make_graph()
+#mst = find_mst_with_kruskal(G)
 
-print(dijkstra_dist(G,0))
-plt.show(G)
+#show_graph(mst)
+u = np.random.randint(GRAPH_SIZE)
+v = np.random.randint(GRAPH_SIZE)
+print("FINDING PATH FROM ",u, " TO ", v)
+approx_path(G,u,v)
+#G = make_and_show_graph
+print("DJIKSTRA FOUND ",dijkstra_dist(G,u)[v])
+print("KRUSKAL FOUND ",approx_path(G,u,v))
+show_graph(G)
+show_graph(mst)
+#plt.show(G)
+#plt.show(mst)
+
 exit()
